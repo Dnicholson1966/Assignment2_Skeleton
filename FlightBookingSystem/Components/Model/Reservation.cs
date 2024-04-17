7a01fbb8393576f4d12ba29b1caf6b10942008d4
Derrick Nicholson <106172127+Dnicholson1966@users.noreply.github.com>
2024-04-17 14:05:32 -0600

    Initiate Repository Assign 2
    Initial Startup of Repository.
    I've learned you can initiate in a tab on the bottom of Visual Studio.

---
+++ FlightBookingSystem/Components/Model/Reservation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightBookingSystem.Components.Model
+{
+    internal class Reservation
+    {
+        internal readonly string _flightCode;
+        internal readonly string _airline;
+        internal readonly string _day;
+        internal readonly string _time;
+        internal readonly decimal _cost;
+        internal readonly string _reservationCode; // Primary Key, generated randomly.
+
+        internal string _passengerName;
+        internal string _passengerCitizenship;
+        
+
+
+        public string FlightCode { get { return _flightCode; } init { _flightCode = value; } }
+        public string Airline { get { return _airline; } init { _airline = value; } }
+        public string Day { get { return _day; } init { _day = value; } }
+        public string Time { get { return _time; } init { _time = value; } }
+        public decimal Cost { get { return _cost; } init { _cost = value; } }
+        public string PassengerName { get { return _passengerName; } set { _passengerName = value; } }
+        public string CitizenShip { get { return _passengerCitizenship; } set { _passengerCitizenship = value; } }
+
+        public string ReservationCode { get { return _reservationCode; } init { _reservationCode = value; } }
+
+        /// <summary>
+        /// Default Constructor for reservation object.
+        /// </summary>
+        /// <param name="flight">Flight assocatied with this reservation.</param>
+        /// <param name="passenger">Passenger's name.</param>
+        /// <param name="citizenship">Passenger's citizenship status.</param>
+        public Reservation (Flight flight, string passenger, string citizenship)
+        {
+            _passengerName = passenger;
+            _passengerCitizenship = citizenship;
+
+            _flightCode = flight.FlightId;
+            _airline = flight.Airline;
+            _day = flight.Day;
+            _time = flight.Time;
+            _cost = flight.Cost;
+
+            // Code to generate Reservation Code. Will do later.
+            /* Random rnd = new Random();
+            int num = rnd.Next(1000); */
+        }
+
+        public override string ToString()
+        {
+            return $"{FlightCode} : {Airline} : {Day} : {Time} : {Cost} : {PassengerName} : {CitizenShip}";
+            // Still needs the Reservation code. Will do later.
+        }
+    }
+}
---
+++ FlightBookingSystem/Components/Repository/DBManager.cs
@@ -0,0 +1,149 @@
+﻿using FlightBookingSystem.Components.Exceptions;
+using FlightBookingSystem.Components.Model;
+using FlightBookingSystem.Components.ViewModel;
+using FlightBookingSystem.Components.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper; // Used mainly for writing to a csv file.
+using System.Globalization; // Used mainly for writing to a csv file.
+
+
+namespace FlightBookingSystem.Components.Repository
+{
+    internal class DBManager
+    {
+        private static readonly string AiportFile = "airports.csv";
+        private static readonly string FlightFile = "flights.csv";
+        private static readonly string ReservationFile = "reservations.csv";
+        private readonly FlightManager flightManager;
+
+
+        // using Singleton design pattern for flight manager class
+        public static DBManager INSTANCE { get; private set; } = new DBManager();
+
+        private DBManager()
+        {
+            flightManager = FlightManager.INSTANCE;
+        }
+
+        public async Task InitializeAsync()
+        {
+
+            loadAirports();
+            loadFlights();
+
+        }
+
+        public void RefreshFlights()
+        {
+            loadFlights();
+        }
+
+        public void RefreshAirports()
+        {
+            loadAirports();
+        }
+
+        private async Task loadAirports()
+        {
+
+            try
+            {
+                using var stream = await FileSystem.OpenAppPackageFileAsync(AiportFile);
+                using var reader = new StreamReader(stream);
+
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    string[] parts = line.Split(",");
+                    flightManager.AddAirport(parts[0], parts[1]);
+                    line = reader.ReadLine();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                // Add handling
+                throw new FileNotFoundException();
+            }
+            catch (NullReferenceException)
+            {
+                // Add handling
+                throw new NullReferenceException();
+            }
+
+        }
+
+
+        private async Task loadFlights()
+        {
+            try
+            {
+                using var stream = await FileSystem.OpenAppPackageFileAsync(FlightFile);
+                using var reader = new StreamReader(stream);
+
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    string[] parts = line.Split(',');
+                    flightManager.AddFlight(parts[0],
+                        parts[1],
+                        GetAirport(parts[2]),
+                        GetAirport(parts[3]),
+                        parts[4],
+                        parts[5],
+                        int.Parse(parts[6]),
+                        decimal.Parse(parts[7]));
+
+                    line = reader.ReadLine();
+
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                // Add handling
+                throw new FileNotFoundException();
+            }
+            catch (NullReferenceException)
+            {
+                // Add handling
+                throw new NullReferenceException()  ;
+            }
+        }
+
+        public async Task WriteReservation(Reservation reservedFlight)
+        {
+                try
+                {
+                using var stream = await FileSystem.OpenAppPackageFileAsync(ReservationFile);
+                using var streamWriter = new StreamWriter(stream);
+                using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+
+                csvWriter.WriteRecord(reservedFlight); // Needs testing, haven't used before.
+                }
+
+            catch (FileNotFoundException)
+            {
+                // Add MORE handling
+                throw new FileNotFoundException();
+            }
+            catch (NullReferenceException)
+            {
+                // Add MORE handling
+                throw new NullReferenceException();
+            }
+        }
+
+
+        private Airport GetAirport(string code)
+        {
+            Airport airport = flightManager.GetAirportByCode(code);
+            return airport == null ? throw new InvalidAirportInfo("Wrong Airport code provided") : airport;
+        }
+    }
+
+
+}
